b5e9b2674894d2c6a3592f615f79a69b6b2ee886
Robert Jansen <rjansengd@gmail.com>
2025-02-25 21:32:12 +0100

    allow emitting raw tokens

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,19 @@ fn main() {
 
     tokenizer.parse();
 
+    if std::env::args().any(|arg| arg.starts_with("--tokens=")) {
+        let token = format!("{:#?}", tokenizer.tokens);
+        let file = std::env::args()
+            .find(|arg| arg.starts_with("--tokens="))
+            .unwrap()[9..]
+            .to_string();
+
+        std::fs::write(&file, token).unwrap();
+        println!("tokens written to {}", file);
+
+        return;
+    }
+
     let mut runtime = runtime::Runtime::new(tokenizer.tokens.clone());
     runtime.run();
 }
